cfec3ce233616ef53dace96c2e9b7c9556af1853
EduardoAyora <eduardoaayora64@gmail.com>
2022-05-25 18:02:04 -0500

    Quitar logs de prisma

--- __tests__/pages/api/horario/[fecha].test.ts
+++ __tests__/pages/api/horario/[fecha].test.ts
@@ -14,10 +14,8 @@ describe('handler', () => {
     await handler(req, res)
 
     expect(res._getStatusCode()).toBe(200)
-    expect(JSON.parse(res._getData())).toEqual(
-      expect.objectContaining({
-        horarioDia: [],
-      })
-    )
+    expect(res._getJSONData()).toEqual({
+      horarioDia: [],
+    })
   })
 })
