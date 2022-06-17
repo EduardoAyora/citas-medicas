05e2909448f53ad86b65dff0560aaf6e19308361
EduardoAyora <eduardoaayora64@gmail.com>
2022-06-17 13:06:23 -0500

    Agregar configuracion de reglas de eslint y prettier para formatear en guardado

--- pages/api/servicio/[servicio]/citas.ts
+++ pages/api/servicio/[servicio]/citas.ts
@@ -18,40 +18,40 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   } = req
 
   switch (method) {
-    case 'POST':
-      if (!servicio || !day || !time)
+  case 'POST':
+    if (!servicio || !day || !time)
+      return res
+        .status(400)
+        .json({ message: 'El servicio el día y la hora son requeridos' })
+
+    try {
+      const servicioId = parseInt(servicio as string)
+      const informacionServicio = await prisma.servicio.findUnique({
+        where: {
+          id: servicioId,
+        },
+      })
+
+      if (!informacionServicio)
         return res
-          .status(400)
-          .json({ message: 'El servicio el día y la hora son requeridos' })
+          .status(404)
+          .json({ message: 'El servicio solicitado no se ha encontrado' })
 
-      try {
-        const servicioId = parseInt(servicio as string)
-        const informacionServicio = await prisma.servicio.findUnique({
-          where: {
-            id: servicioId,
-          },
-        })
+      const citaCreada = await prisma.cita.create({
+        data: {
+          day,
+          time,
+          durationInMinutes: informacionServicio.duracionEnMinutos,
+          servicioId,
+        },
+      })
 
-        if (!informacionServicio)
-          return res
-            .status(404)
-            .json({ message: 'El servicio solicitado no se ha encontrado' })
-
-        const citaCreada = await prisma.cita.create({
-          data: {
-            day,
-            time,
-            durationInMinutes: informacionServicio.duracionEnMinutos,
-            servicioId,
-          },
-        })
-
-        return res.status(200).json({ cita: citaCreada })
-      } catch (error) {
-        return res.status(500).json({ message: 'Ha ocurrido un error' })
-      }
-    default:
-      return res.status(405).end(`Method ${method} Not Allowed`)
+      return res.status(200).json({ cita: citaCreada })
+    } catch (error) {
+      return res.status(500).json({ message: 'Ha ocurrido un error' })
+    }
+  default:
+    return res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
 
---
+++ src/components/layout/Layout.test.tsx
@@ -0,0 +1,5 @@
+import Layout from './Layout'
+
+describe('Layout', () => {
+  test('Renderiza el layout con los enlaces en la barra de navegación', () => {})
+})
