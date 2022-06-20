1a8ff4eff317b6d6622fced6ebc953bffb92f68d
EduardoAyora <eduardoaayora64@gmail.com>
2022-06-20 14:53:02 -0500

    Corregir pagina index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
-import NuevaCita from '../src/components/nueva-cita/NuevaCita'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <NuevaCita />
+    <div className='flex h-screen justify-center items-center'>
+      <Link href='/login'>
+        <span className='underline text-blue-600 cursor-pointer'>Login</span>
+      </Link>
     </div>
   )
 }
