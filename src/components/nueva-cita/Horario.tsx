ba45d9179180b2c325eeac10a1a7940279a75932
EduardoAyora <eduardoaayora64@gmail.com>
2022-06-22 23:13:12 -0500

    Agregar estilo para escoger horario

--- src/components/nueva-cita/Horario.tsx
+++ src/components/nueva-cita/Horario.tsx
@@ -7,11 +7,20 @@ interface Props {
 
 const HorarioDia: React.FC<Props> = ({ availableHours, setHour }) => {
   return (
-    <div>
-      <ul>
+    <div className='mt-8 flex flex-col text-center sm:mt-0 sm:w-1/3 sm:pl-4 md:-mb-5'>
+      <div className='mb-4 text-left text-lg font-light text-gray-600'>
+        <span className='text-bookingdarker w-1/2'>
+          <strong>Thursday</strong>
+          <span className='text-bookinglight'>, 23 June</span>
+        </span>
+      </div>
+      <ul className='flex-grow overflow-y-auto md:h-[364px]'>
         {availableHours.map((availableHour, indice) => (
           <li key={indice}>
-            <button onClick={() => setHour(availableHour)}>
+            <button
+              onClick={() => setHour(availableHour)}
+              className='text-gray-900 w-full hover:bg-brand hover:text-brandcontrast mb-2 block rounded-sm border-2 bg-white py-4 font-medium hover:text-white hover:bg-black border-black'
+            >
               {availableHour}
             </button>
           </li>
