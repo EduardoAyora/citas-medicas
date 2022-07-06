1c214c4a553452e768df39d4cd4d8befd5364f51
EduardoAyora <eduardoaayora64@gmail.com>
2022-07-05 22:59:49 -0500

    Agregar icono de carga

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,9 @@
-const Loading = () => {
+const Loading = ({ isDarkMode = true }) => {
   return (
     <svg
-      className='mt-[-9px] mr-1 inline h-5 w-5 animate-spin text-black dark:text-white'
+      className={`mt-[-9px] mr-1 inline h-5 w-5 animate-spin text-black ${
+        isDarkMode && 'dark:text-white'
+      }`}
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
       viewBox='0 0 24 24'
@@ -20,7 +22,7 @@ const Loading = () => {
         d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
       ></path>
     </svg>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
