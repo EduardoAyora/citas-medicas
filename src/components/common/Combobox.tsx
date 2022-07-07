dacd750403564b35bea96e8e87cf442f319fda32
EduardoAyora <eduardoaayora64@gmail.com>
2022-07-07 18:02:01 -0500

    Corregir estilos combobox

--- src/components/common/Combobox.tsx
+++ src/components/common/Combobox.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useState } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronDownIcon } from '@heroicons/react/solid'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
 interface Props {
   options: { value: string; text: string }[]
@@ -23,8 +23,8 @@ const MyCombobox: React.FC<Props> = ({ options }) => {
   return (
     <div>
       <Combobox value={selected} onChange={setSelected}>
-        <div className='relative z-10 mt-1'>
-          <div className='relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
+        <div className='text-sm shadow-sm w-[120px] z-10 relative'>
+          <div className='flex cursor-default text-gray-700 border items-center h-10 px-2 justify-between border-gray-300 hover:border-gray-400'>
             <Combobox.Input
               className='w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
               displayValue={(option: { text: string }) => option.text}
@@ -32,7 +32,7 @@ const MyCombobox: React.FC<Props> = ({ options }) => {
             />
             <Combobox.Button className='absolute inset-y-0 right-0 flex items-center pr-2'>
               <ChevronDownIcon
-                className='h-6 w-6 text-gray-400'
+                className='h-6 w-6 text-gray-400 hover:text-gray-500'
                 aria-hidden='true'
               />
             </Combobox.Button>
@@ -47,39 +47,24 @@ const MyCombobox: React.FC<Props> = ({ options }) => {
             <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
               {filteredPeople.length === 0 && query !== '' ? (
                 <div className='relative cursor-default select-none py-2 px-4 text-gray-700'>
-                  Nothing found.
+                  No se encontró.
                 </div>
               ) : (
                 filteredPeople.map((option) => (
                   <Combobox.Option
                     key={option.value}
-                    className={({ active }) =>
+                    className={({ active, selected }) =>
                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                        active ? 'bg-gray-100' : 'text-gray-900'
+                      } ${
+                        selected
+                          ? 'font-medium text-white bg-black'
+                          : 'font-normal'
                       }`
                     }
                     value={option}
                   >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                          }`}
-                        >
-                          {option.text}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-teal-600'
-                            }`}
-                          >
-                            <CheckIcon className='h-5 w-5' aria-hidden='true' />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
+                    {option.text}
                   </Combobox.Option>
                 ))
               )}
@@ -92,26 +77,3 @@ const MyCombobox: React.FC<Props> = ({ options }) => {
 }
 
 export default MyCombobox
-
-// ;<div className='text-sm shadow-sm w-[120px]'>
-//   <span className=''></span>
-//   <div className='flex cursor-default text-gray-700 border items-center h-10 px-2 justify-between border-gray-300 hover:border-gray-400'>
-//     <div className=''>
-//       <div className=''>9:00am</div>
-//     </div>
-//     <div className=''>
-//       <div className='' aria-hidden='true'>
-//         <svg
-//           height='20'
-//           width='20'
-//           viewBox='0 0 20 20'
-//           aria-hidden='true'
-//           focusable='false'
-//           className='fill-gray-400 hover:fill-gray-500'
-//         >
-//           <path d='M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z'></path>
-//         </svg>
-//       </div>
-//     </div>
-//   </div>
-// </div>
